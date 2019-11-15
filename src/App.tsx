7e9bd34566e47b420a873af1bec26ec08683f01d
Aubrey Holland <aubreyholland@gmail.com>
2019-11-15 09:03:13 -0500

    yay

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Honeybadger from 'honeybadger-js';
 
 function App() {
+  const config = {
+    apiKey: process.env.REACT_APP_HONEYBADGER_API_KEY || '',
+    environment: process.env.NODE_ENV,
+    revision: process.env.REACT_APP_SOURCE_VERSION
+  };
+  
+  Honeybadger.configure(config)
+
+  const handleClickI = () => {
+    Honeybadger.notify(new Error('sdfsdf'));
+  };
+
+  const handleClickII = () => {
+    Honeybadger.setContext({ userId: 'dfsdfsdf' });
+  }
+
   return (
     <div className="App">
+      <button onClick={handleClickI}>BUTTON</button>
+      <button onClick={handleClickII}>OTHER BUTTON</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
